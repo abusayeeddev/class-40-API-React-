9c546a7841736e79ad545a9fb6220498dd9ceb15
Abu Sayeed <abusayeed483698@gmail.com>
2026-04-07 14:55:11 -0500

    Update Container.jsx

--- src/Components/Common/Container.jsx
+++ src/Components/Common/Container.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 
 const Container = ({ children, className }) => {
@@ -8,14 +8,4 @@ const Container = ({ children, className }) => {
 }
 
 export default Container
-=======
-import React from 'react'
 
-const Container = ({ children, className }) => {
-  return (
-    <div className={`w-330 mx-auto ${className}`}>{children}</div>
-  )
-}
-
-export default Container
->>>>>>> f2c0f3bf682d69f5c539a4b7a36953d8361939cb
