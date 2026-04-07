73d9a7af33771652fbd3650ac931794383eefc4a
Abu Sayeed <abusayeed483698@gmail.com>
2026-04-07 15:02:09 -0500

    Update Error.jsx

--- src/Components/Page/Error.jsx
+++ src/Components/Page/Error.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 
 const Error = () => {
@@ -11,17 +11,4 @@ const Error = () => {
 }
 
 export default Error
-=======
-import React from 'react'
 
-const Error = () => {
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
-  )
-}
-
-export default Error
->>>>>>> f2c0f3bf682d69f5c539a4b7a36953d8361939cb
