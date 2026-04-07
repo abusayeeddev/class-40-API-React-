35a400da8d61f7cd4c598f332c007331805d6ea2
Abu Sayeed <abusayeed483698@gmail.com>
2026-04-07 14:54:52 -0500

    Update Buttons.jsx

--- src/Components/Common/Buttons.jsx
+++ src/Components/Common/Buttons.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { Children } from 'react'
 
 const Buttons = ({ children }) => {
@@ -8,14 +8,4 @@ const Buttons = ({ children }) => {
 }
 
 export default Buttons
-=======
-import React, { Children } from 'react'
 
-const Buttons = ({ children }) => {
-  return (
-    <button className={`py-4 px-5 bg-mainclr text-txt hover:bg-secondary duration-200 font-DMsans`}>{children}</button>
-  )
-}
-
-export default Buttons
->>>>>>> f2c0f3bf682d69f5c539a4b7a36953d8361939cb
