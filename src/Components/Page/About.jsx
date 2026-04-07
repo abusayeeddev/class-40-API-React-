cc49ab81d7af963b291049cfafc24bc29625aea2
Abu Sayeed <abusayeed483698@gmail.com>
2026-04-07 15:01:36 -0500

    Update About.jsx

--- src/Components/Page/About.jsx
+++ src/Components/Page/About.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 
 const About = () => {
@@ -10,16 +10,4 @@ const About = () => {
 }
 
 export default About
-=======
-import React from 'react'
 
-const About = () => {
-  return (
-    <div>
-      <h1 className='text-blue-500'>Welcome to the About Page!</h1>
-    </div>
-  )
-}
-
-export default About
->>>>>>> f2c0f3bf682d69f5c539a4b7a36953d8361939cb
