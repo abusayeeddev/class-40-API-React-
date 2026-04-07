2080540e0320aad7e1e7fe0627adfc3dc29030f8
Abu Sayeed <abusayeed483698@gmail.com>
2026-04-07 14:55:29 -0500

    Update Flex.jsx

--- src/Components/Common/Flex.jsx
+++ src/Components/Common/Flex.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 
 const Flex = ({ className, children }) => {
@@ -10,16 +10,4 @@ const Flex = ({ className, children }) => {
 }
 
 export default Flex
-=======
-import React from 'react'
 
-const Flex = ({ className, children }) => {
-  return (
-    <div className={`flex items-center ${className}`}>
-        {children}
-    </div>
-  )
-}
-
-export default Flex
->>>>>>> f2c0f3bf682d69f5c539a4b7a36953d8361939cb
