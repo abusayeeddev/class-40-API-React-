9f26c771555fc0f585466545c28926dfc2d81413
Abu Sayeed <abusayeed483698@gmail.com>
2026-04-07 14:55:43 -0500

    Update Images.jsx

--- src/Components/Common/Images.jsx
+++ src/Components/Common/Images.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 
 const Images = ({ src, alt, className }) => {
@@ -8,14 +8,4 @@ const Images = ({ src, alt, className }) => {
 }
 
 export default Images
-=======
-import React from 'react'
 
-const Images = ({ src, alt, className }) => {
-  return (
-    <img src={src} alt={alt} className={`${className}`} />
-  )
-}
-
-export default Images
->>>>>>> f2c0f3bf682d69f5c539a4b7a36953d8361939cb
